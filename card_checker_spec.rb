a4ea84f5a6c36ebc9fe66ae3c25cab639551c5f7
lisichkin <lisichkin@elins.ru>
2019-11-20 14:21:42 +0300

    add failed test form card number with spaces

--- card_checker_spec.rb
+++ card_checker_spec.rb
@@ -11,5 +11,9 @@ RSpec.describe CardChecker do
     it 'returns false for invalid card' do
       expect(described_class.valid?('4561261212345464')).to be_falsey
     end
+
+    it 'returns true for valid card with spaces' do
+      expect(described_class.valid?('4561 2612 1234 5467')).to be_truthy
+    end
   end
 end
